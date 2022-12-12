1ccec49ae2acf2661aef59cbe0d2868bb491ed2c
tmao123 <tmao12345678@gmail.com>
2022-12-12 11:05:36 +0800

    221212 table init

--- app/home/views.py
+++ app/home/views.py
@@ -1,1 +1,16 @@
 from flask import render_template, Blueprint
+
+home = Blueprint("home", __name__,
+                 template_folder="templates",
+                 static_folder="static",
+                 static_url_path='/home/static')
+
+
+@home.route("/")
+def index():
+    return render_template("home/home.html")
+
+
+@home.route('/about', methods=['GET'])
+def about():
+    return render_template("home/about.html")
